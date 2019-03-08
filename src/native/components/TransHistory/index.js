ae36a0544e44c3e918d25ecdb170eadce893c955
LAZYSHOUTDESK\user <tim70036@gmail.com>
2019-03-08 16:42:08 +0800

    no message

--- src/native/components/TransHistory/index.js
+++ src/native/components/TransHistory/index.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
 import { Table, Row } from 'react-native-table-component';
 
+import { viewportWidthPercent, viewportHeightPercent } from '../../lib/util';
+
+const tableHeads = ['時間', '對象', '數量', '備註'];
+const tableColWidths = [100, 60, 120, 100];
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -11,14 +16,14 @@ const styles = StyleSheet.create({
   },
   header: {
     height: 50,
-    backgroundColor: '#537791'
+    backgroundColor: '#537791',
   },
   text: {
     textAlign: 'center',
     fontWeight: '100',
   },
   dataWrapper: {
-    marginTop: -1
+    marginTop: -1,
   },
   row: {
     height: 40,
@@ -30,37 +35,28 @@ class TransHistory extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tableHead: ['時間', '對象', '數量', '備註', ],
-      widthArr: [40, 60, 80, 100],
-    }
+      
+    };
   }
 
   render = () => {
-    const { tableHead, widthArr } = this.state;
-    const tableData = [];
-    for (let i = 0; i < 30; i += 1) {
-      const rowData = [];
-      for (let j = 0; j < 4; j += 1) {
-        rowData.push(`${i}${j}`);
-      }
-      tableData.push(rowData);
-    }
+    const { tableData } = this.props;
 
     return (
       <View style={styles.container}>
         <ScrollView horizontal>
           <View>
             <Table borderStyle={{ borderColor: '#C1C0B9' }}>
-              <Row data={tableHead} widthArr={widthArr} style={styles.header} textStyle={styles.text} />
+              <Row data={tableHeads} widthArr={tableColWidths} style={styles.header} textStyle={styles.text} />
             </Table>
             <ScrollView style={styles.dataWrapper}>
               <Table borderStyle={{ borderColor: '#C1C0B9' }}>
                 {
                   tableData.map((rowData, index) => (
                     <Row
-                      key={index}
+                      key={index*10}
                       data={rowData}
-                      widthArr={widthArr}
+                      widthArr={tableColWidths}
                       style={[styles.row, index%2 && { backgroundColor: '#F7F6E7' }]}
                       textStyle={styles.text}
                     />
