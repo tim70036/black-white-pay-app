42eb89622b8a9c6c3f35ad5ed29ca397dbe2c941
LAZYSHOUTDESK\user <tim70036@gmail.com>
2019-03-09 15:33:57 +0800

    TransHistory rebuild

--- src/native/components/TransHistory/index.js
+++ src/native/components/TransHistory/index.js
@@ -1,95 +1,139 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
-import { Table, Row } from 'react-native-table-component';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { Icon } from 'native-base';
 import PropTypes from 'prop-types';
+import Accordion from 'react-native-collapsible/Accordion';
 
 import { viewportWidthPercent, viewportHeightPercent } from '../../lib/util';
 
-const tableHeads = ['時間', '對象', '數量', '備註'];
-const tableColWidths = [120, 120, 150, 120];
+const tablePaddingHorizontal = viewportWidthPercent(2);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: viewportWidthPercent(2),
     paddingTop: 30,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 1,
+    paddingVertical: 10,
+    paddingHorizontal: tablePaddingHorizontal,
     backgroundColor: '#fff',
   },
-  header: {
-    height: 50,
+  headerTime: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
-  headerBorder: {
-    borderColor: '#C1C0B9',
-    borderWidth: 1.8,
+  headerAmount: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  text: {
-    textAlign: 'center',
-    fontWeight: '100',
+  headerText: {
+    fontWeight: '200',
   },
-  dataWrapper: {
-    marginTop: -1,
+  headerIcon: {
   },
-  row: {
-    height: 40,
+  contentContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: viewportWidthPercent(25),
+    backgroundColor: '#fff',
+    paddingHorizontal: tablePaddingHorizontal,
   },
-  rowBorder: {
-    borderColor: '#C1C0B9',
+  contentLeft: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    paddingHorizontal: viewportWidthPercent(2),
+  },
+  contentRight: {
+    flex: 7,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    paddingHorizontal: viewportWidthPercent(2),
   },
 });
 
+
 class TransHistory extends Component {
   static propTypes = {
-    tableData: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)),
+    historyData: PropTypes.arrayOf(
+      PropTypes.shape({
+        time: PropTypes.string,
+        relatedName: PropTypes.string,
+        amount: PropTypes.string,
+        comment: PropTypes.string,
+      }),
+    ),
   };
 
   static defaultProps ={
-    tableData: [],
+    historyData: [],
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
+  state = {
+    activeSections: [],
+  };
 
-    };
-  }
-
-  _renderRow = () => {
-    const { tableData } = this.props;
-
-    const rows = tableData.map((rowData, index) => (
-      <Row
-        key={index*10}
-        data={rowData}
-        widthArr={tableColWidths}
-        style={[styles.row, index % 2 && { backgroundColor: '#f2f2f2' }]}
-        textStyle={styles.text}
-      />
-    ));
-
-    return rows;
-  }
-
-  render = () => (
-    <View style={styles.container}>
-      <ScrollView horizontal>
-        <View>
-          <Table borderStyle={styles.headerBorder}>
-            <Row
-              data={tableHeads}
-              widthArr={tableColWidths}
-              style={styles.header}
-              textStyle={styles.text}
-            />
-          </Table>
-          <ScrollView style={styles.dataWrapper}>
-            <Table borderStyle={styles.rowBorder}>
-              {this._renderRow()}
-            </Table>
-          </ScrollView>
-        </View>
-      </ScrollView>
+  _renderHeader = section => (
+    <View style={styles.headerContainer}>
+      <View style={styles.headerTime}>
+        <Text style={styles.headerText}>{section.time}</Text>
+      </View>
+      <View style={styles.headerAmount}>
+        <Text style={styles.headerText}>{section.amount}</Text>
+        <Icon type="FontAwesome" name="angle-down" style={styles.headerIcon} />
+      </View>
     </View>
   );
+
+  _renderContent = section => (
+    <View style={styles.contentContainer}>
+      <View style={styles.contentLeft}>
+        <Text>對象:</Text>
+        <Text>備註:</Text>
+      </View>
+      <View style={styles.contentRight}>
+        <Text>{section.relatedName}</Text>
+        <Text>{section.comment}</Text>
+      </View>
+    </View>
+  );
+
+  _updateSections = (activeSections) => {
+    this.setState({ activeSections });
+  };
+
+  render = () => {
+
+    const { activeSections } = this.state;
+    const { historyData } = this.props;
+    return (
+      <ScrollView style={styles.container}>
+        <View style={[styles.headerContainer, { marginVertical: 2, backgroundColor: '#cce6ff' }]}>
+          <View style={styles.headerTime}>
+            <Text style={styles.headerText}>交易時間</Text>
+          </View>
+          <View style={styles.headerAmount}>
+            <Text style={styles.headerText}>轉帳數量</Text>
+          </View>
+        </View>
+        <Accordion
+          sections={historyData}
+          activeSections={activeSections}
+          renderHeader={this._renderHeader}
+          renderContent={this._renderContent}
+          onChange={this._updateSections}
+        />
+      </ScrollView>
+    );
+  };
 }
 
 export default TransHistory;
